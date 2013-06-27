80cc5ad196dc11dff22081f120dae6e4fd0b6e9a
kirchnermarc <marc.kirchner@intentmedia.com>
2013-06-27 01:43:13 -0400

    submission of hw2

--- hw2/hw2.r
+++ hw2/hw2.r
@@ -3,71 +3,58 @@
 # Author: Marc Kirchner    Date: 2013 06 23
 # 
 # Regression assignment: predict salaries based on job descriptions
-#
-# Note: I couldn't use the big files for training. My computer was taking too 
-# long to even load the CSVs.
-# As an exercise, I just used the small training data set. 
-# I selected a limited number of features to test the different outputs. 
-# I joined the location tree and also selected a limited number of locations 
-# to be used as features to improve the model. 
-# So I don't submit predictions, just the R code I've been using. 
+# Note: I had problems with memory allocation and processing power 
+# when trying to use files bigger than train.csv. Example: when
+# merging with the location tree. Even with that file I had to trim down 
+# the data a bit to avoid memory allocation problems when using glmnet. 
 # ==============================================================================
+#Define mean Absolute Error function
+mae <- function(x, y) {
+  mean( abs(x-y) )
+}
 
 setwd('~/general_assembly/data_science/git/cloned/arahuja/GADS4/data/kaggle_salary')
+salaries <- read.csv('train.csv', header=TRUE)
+#names(salaries)
+#head(salaries)
+
+
+#We add columns for specific words showing in some of the original columns
+salaries$LondonInDescription <- grepl("London", salaries$FullDescription)
+salaries$CompetitiveInDescription <- grepl("Competitive", salaries$FullDescription)
+salaries$BenefitsInDescription <- grepl("Benefits", salaries$FullDescription)
+
+#For memory reasons I decided to drop some of the original columns. In a real scenario I would dedicate 
+#more time and resources to try to extract as many features as possible from the data
+salaries <- subset(salaries, select = -c(FullDescription, SalaryRaw, LocationRaw, Title, Company, SourceName))
 
 # --------------------------------------------
 # 1 - Split the data into training and test sets
 # --------------------------------------------
-salaries <- read.csv('train.csv', header=TRUE)
-#names(salaries)
-
-#Generate random index
 train.idx <- sample(1: nrow(salaries), .7*nrow(salaries))
-
-#Extract features from the raw data
-#We have 28 different job categories exist in the data
-unique(salaries$Category)
-
-#Create a feature for some categories
-salaries$Engineering <- grepl("Engineering", salaries$Category)
-salaries$Accounting <- grepl("Accounting", salaries$Category)
-salaries$IT <- grepl("IT", salaries$Category)
-salaries$Hospitality <- grepl("Hospitality", salaries$Category)
-salaries$Graduate <- grepl("Graduate", salaries$Category)
-#Permanent Contract?
-salaries$Permanent <- salaries$ContractTime == 'permanent'
-#Location is in London?
-salaries$London <- grepl("London", salaries$LocationRaw) 
-
-#Training / Test sets
 training <- salaries[train.idx,]
 test <- salaries[-train.idx,]
 
 # ----------------------------------------
 # 2 - Build simple linear regression model
 # ----------------------------------------
-#Define mean Absolute Error function
-mae <- function(x, y) {
-  mean( abs(x-y) )
-}
-
 #Define simple model and test it
-model <- lm(SalaryNormalized ~  Engineering + Accounting + IT + Hospitality + Graduate + Permanent + London, data=training)
+model <- lm(SalaryNormalized ~  Category + ContractType + ContractTime + LondonInDescription + CompetitiveInDescription + BenefitsInDescription, data=training)
 test.predict <- predict(model, test)
 
 summary(model)
 mae(test.predict, test$SalaryNormalized)
 #For this model we get
-# ~0.174  R Squared
-# ~10235   MAE
+# ~0.21    R Squared
+# ~10200   MAE
 
 
 # --------------------------------------------
 # 3 - Build model with cross validation: cv.lm
 # --------------------------------------------
-install.packages('DAAG')
-library('DAAG')
-model.cv <- cv.lm(df=training, form.lm = formula(model), m=4)
+#install.packages('DAAG')
+#library('DAAG')
+#model.cv <- cv.lm(df=training, form.lm = formula(model), m=4)
 #How to calculate MAE?
 
 
@@ -76,39 +63,63 @@ model.cv <- cv.lm(df=training, form.lm = formula(model), m=4)
 # 4 - Merge Location_Tree.csv
 # ---------------------------
 location.tree <- read.csv('~/general_assembly/data_science/git/kircm/ga_data_science/hw2/Location_Tree2.csv', header=FALSE)
+
+# Merge the data
 salaries_location <- merge(salaries, location.tree, by.x="LocationNormalized", by.y="V3")
 
-unique(salaries_location$V2)
-
-salaries_location$Engineering <- grepl("Engineering", salaries_location$Category)
-salaries_location$Accounting <- grepl("Accounting", salaries_location$Category)
-salaries_location$IT <- grepl("IT", salaries_location$Category)
-salaries_location$Hospitality <- grepl("Hospitality", salaries_location$Category)
-salaries_location$Graduate <- grepl("Graduate", salaries_location$Category)
-#Permanent Contract?
-salaries_location$Permanent <- salaries_location$ContractTime == 'permanent'
-#Location is in London?
-salaries_location$London <- salaries_location$V2 == 'London'
-#Location is in Scotland?
-salaries_location$Scotland <- salaries_location$V2 == 'Scotland'
-
-#Location is in Northern Ireland?
-salaries_location$NorthernIreland <- salaries_location$V2 == 'Northern Ireland'
-#Location is in South East England ?
-salaries_location$SouthEastEngland  <- salaries_location$V2 == 'South East England'
-
+# Model
 train.idx <- sample(1: nrow(salaries_location), .7*nrow(salaries_location))
-training <- salaries_location[train.idx,]
-test <- salaries_location[-train.idx,]
+training_merged <- salaries_location[train.idx,]
+test_merged <- salaries_location[-train.idx,]
 
-model <- lm(SalaryNormalized ~  Engineering + Accounting + IT + Hospitality + Graduate + Permanent + London + Scotland + NorthernIreland + SouthEastEngland, data=training)
-test.predict <- predict(model, test)
+model <- lm(SalaryNormalized ~  Category + ContractTime +  Category + ContractType + ContractTime + LondonInDescription + CompetitiveInDescription + BenefitsInDescription + V2, data=training_merged)
+test.predict <- predict(model, test_merged)
 
 summary(model)
+mae(test.predict, test_merged$SalaryNormalized)
+#For this model we get
+# ~0.25  R Squared
+# ~9500    MAE
+
+
+# ---------------------------
+# 5 - Use glmnet
+# Note: I had to remove some features to avoid memory allocation problems
+# I couldn't use the merged data either
+# ---------------------------
+library(glmnet)
+model <- cv.glmnet(model.matrix(~training$Category:training$ContractTime:training$Category:training$ContractType:training$ContractTime:training$LondonInDescription), matrix(training$SalaryNormalized))
+test.predict <- as.vector(predict(model, model.matrix(~test$Category:test$ContractTime:test$Category:test$ContractType:test$ContractTime:test$LondonInDescription), s="lambda.min"))
 mae(test.predict, test$SalaryNormalized)
 #For this model we get
-# ~0.176  R Squared
-# ~9956    MAE
+# ~10000    MAE
+
+
+
+# ------------------
+# Output predictions
+# ------------------
+# When the exercise is done, make preductions on the actual test data and submit
+realtest <- read.csv("test.csv")
+
+#Some category values are present in the realtest but missing in the small training file
+for (j in c("Category")) {
+  i = which( !(realtest[[j]] %in% levels(training[[j]])))
+  realtest[i,j] <- NA
+}
+
+#Add the extra columns we created in training
+realtest$LondonInDescription <- grepl("London", realtest$FullDescription)
+realtest$CompetitiveInDescription <- grepl("Competitive", realtest$FullDescription)
+realtest$BenefitsInDescription <- grepl("Benefits", realtest$FullDescription)
+
+
+predictions <- predict(model, realtest)
+submission <- data.frame(Id=realtest$Id, Salary=predictions)
+write.csv(submission, file = "submission.csv")
+
+
+
 
 
 
